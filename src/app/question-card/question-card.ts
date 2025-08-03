19913079eb6d784f24369a503ef2a893f558ef6f
fran0770 <franmowat33@gmail.com>
2025-08-03 20:35:52 +0100

    removed redundant question component

--- src/app/question-card/question-card.ts
+++ src/app/question-card/question-card.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { Question } from "../question/question";
 import { AnswerOption } from "../answer-option/answer-option";
 
 @Component({
   selector: 'app-question-card',
-  imports: [Question, AnswerOption],
+  imports: [AnswerOption],
   templateUrl: './question-card.html',
   styleUrl: './question-card.scss'
 })
