b0ad4eb215eeaeb2635aafd774ee14d0ef522c9c
fran0770 <franmowat33@gmail.com>
2025-08-10 17:45:41 +0100

    updated answer options on selection

--- src/app/question-card/question-card.ts
+++ src/app/question-card/question-card.ts
@@ -11,4 +11,16 @@ export class QuestionCard {
   optionNumberValues: Array<string> = ["A", "B", "C", "D"];
   questionValue: string = "question...";
   answerValues: Array<string> = ["option 1", "number 2", "3", "last answer"];
+  selectedAnswer: number = 0; 
+
+  changeSelectedAnswer(newSelection: number): void {
+    const options = document.getElementsByTagName("app-answer-option");
+
+    const oldSelectedAnswer = options[this.selectedAnswer].firstChild as HTMLElement;
+    oldSelectedAnswer.classList.remove("selected");
+
+    this.selectedAnswer = newSelection;
+    const newSelectedAnswer = options[this.selectedAnswer].firstChild as HTMLElement; 
+    newSelectedAnswer.classList.add("selected");
+  }
 }
