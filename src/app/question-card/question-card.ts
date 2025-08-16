4608f11a0ffd8c47d4b7c65dc22f783e217f87e6
fran0770 <franmowat33@gmail.com>
2025-08-16 16:31:25 +0100

    obtained token from api

--- src/app/question-card/question-card.ts
+++ src/app/question-card/question-card.ts
@@ -12,24 +12,27 @@ export class QuestionCard {
   optionNumberValues: Array<string> = ["A", "B", "C", "D"];
   questionValue: string = "question...";
   answerValues: Array<string> = ["option 1", "number 2", "3", "last answer"];
-  selectedAnswer: number = 0; 
+  answerSelected: boolean = false; 
   score: number = 0;
   questionNumber: number = 0; 
   questionCount: number = 10; 
   state: "ready" | "answered" = "ready";
 
   selectAnswer(answerNumber: number): void {
-    this.selectedAnswer = answerNumber;
-    this.state = "answered";
-    const options = document.getElementsByTagName("app-answer-option");
+    if (! this.answerSelected){
+      this.answerSelected = true;
+      this.state = "answered";
+      const options = document.getElementsByTagName("app-answer-option");
 
-    for (let i = 0; i < options.length; i++){
-      let answerOption = options[i].firstChild as HTMLElement; 
-      if (i === this.selectedAnswer){
-        answerOption.classList.add("correct");
-      } else {
-        answerOption.classList.add("disabled");
-      }
+      for (let i = 0; i < options.length; i++){
+        let answerOption = options[i].firstChild as HTMLElement; 
+        if (i === answerNumber){
+          answerOption.classList.add("correct");
+          this.score++;
+        } else {
+          answerOption.classList.add("disabled");
+        }
+      };
     };
   };
 
@@ -37,11 +40,22 @@ export class QuestionCard {
     this.state = "ready";
     this.answerValues = ["new option 1", "new number 2", "new 3", "new no 4"];
     this.questionNumber++; 
+    this.answerSelected = false; 
 
     const options = document.getElementsByTagName("app-answer-option");
     Array.from(options).forEach(answerOption => {
       let answerValue = answerOption.firstChild as HTMLElement; 
       answerValue.classList = "";
     });
+  };
+
+  async getToken(): Promise<string> {
+    return fetch("https://opentdb.com/api_token.php?command=request")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return data["token"];
+    })
   };
 };
