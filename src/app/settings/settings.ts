976f2e1ca9606d9bd04d1dc9d94d7aa830a5ec67
fran0770 <franmowat33@gmail.com>
2025-08-17 16:37:22 +0100

    connected html form controls to ts variables

--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './settings.html',
   styleUrl: './settings.scss'
 })
@@ -10,4 +12,18 @@ export class Settings {
   questionCount: number = 15; 
   category: string = "any";
   difficulty: string = "mixed";
-} 
+
+  categoryOptions = [
+    {id: "any", name: "Any Category"}, 
+    {id: "generalKnowledge", name: "General Knowledge"}, 
+    {id: "sports", name: "Sports"}, 
+    {id: "geography", name: "Geography"}, 
+    {id: "history", name: "History"}, 
+    {id: "science", name: "Science & Nature"}, 
+    {id: "computers", name: "Computers"}, 
+    {id: "mathematics", name: "Mathematics"}, 
+    {id: "politics", name: "Politics"}, 
+    {id: "mythology", name: "Mythology"}, 
+    {id: "animals", name: "Animals"}
+  ];
+};
