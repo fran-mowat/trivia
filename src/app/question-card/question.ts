aa2604cd7d28460568e6ce5f0f81c09aaea22c36
fran0770 <franmowat33@gmail.com>
2025-08-17 09:14:00 +0100

    created interface for question objects returned by the API

---
+++ src/app/question-card/question.ts
@@ -0,0 +1,8 @@
+export interface Question {
+    category: string,
+    correct_answer: string,
+    difficulty: string, 
+    incorrect_answers: Array<string>,
+    question: string, 
+    type: string, 
+};
