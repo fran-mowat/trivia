2337de4367fa0d1a3970afb94a6b9a01278a5be2
fran0770 <franmowat33@gmail.com>
2025-08-17 19:59:00 +0100

    generated summary card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QuestionCard } from "./question-card/question-card";
 import { Settings } from "./settings/settings";
+import { SummaryCard } from './summary-card/summary-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, QuestionCard, Settings],
+  imports: [RouterOutlet, QuestionCard, Settings, SummaryCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
