2f49dc804cf44458d7de87bd31c3b656f1c81a0b
fran0770 <franmowat33@gmail.com>
2025-08-30 16:53:46 +0100

    created unit tests for summary card component

--- src/app/summary-card/summary-card.spec.ts
+++ src/app/summary-card/summary-card.spec.ts
@@ -14,10 +14,36 @@ describe('SummaryCard', () => {
 
     fixture = TestBed.createComponent(SummaryCard);
     component = fixture.componentInstance;
+    component.category = "Mathematics";
+    component.difficulty = "mixed";
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display quiz settings', () => {
+    const categoryDisplay = fixture.nativeElement.querySelectorAll('span')[0];
+    const difficultyDisplay = fixture.nativeElement.querySelectorAll('span')[1];
+
+    expect(categoryDisplay.innerHTML).toBe('mathematics');
+    expect(difficultyDisplay.innerHTML).toBe('mixed');
+
+    component.category = "Politics";
+    component.difficulty = "hard";
+    fixture.detectChanges();
+
+    expect(categoryDisplay.innerHTML).toBe('politics');
+    expect(difficultyDisplay.innerHTML).toBe('hard');
+  });
+
+  it('should play again', () => {
+    const spy = spyOn(component.playAgain, 'emit');
+
+    const button = fixture.nativeElement.querySelector('input');
+    button.dispatchEvent(new Event('click'));
+
+    expect(spy).toHaveBeenCalled();
   });
 });
