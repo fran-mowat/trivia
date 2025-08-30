28b08049bfc695f9531b84e8df18e95d8c3e7a5a
fran0770 <franmowat33@gmail.com>
2025-08-30 15:01:05 +0100

    created unit tests for app root

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,37 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render the settings card', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, trivia');
+    
+    expect(compiled.querySelector('app-settings')).toBeTruthy();
+    expect(compiled.querySelector('app-question-card')).toBeFalsy();
+    expect(compiled.querySelector('app-summary-card')).toBeFalsy();
+  });
+
+  it('should render the question card', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+    
+    fixture.componentInstance.screenMode = "questions";
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('app-settings')).toBeFalsy();
+    expect(compiled.querySelector('app-question-card')).toBeTruthy();
+    expect(compiled.querySelector('app-summary-card')).toBeFalsy();
+  });
+
+  it('should render the summary card', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+    
+    fixture.componentInstance.screenMode = "review";
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('app-settings')).toBeFalsy();
+    expect(compiled.querySelector('app-question-card')).toBeFalsy();
+    expect(compiled.querySelector('app-summary-card')).toBeTruthy();
   });
 });
