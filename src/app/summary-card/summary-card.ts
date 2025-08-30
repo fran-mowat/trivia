fe74321fde4d91925a9f6671be084857597a381b
fran0770 <franmowat33@gmail.com>
2025-08-30 13:31:39 +0100

    added corrections to progress bar for boundary values

--- src/app/summary-card/summary-card.ts
+++ src/app/summary-card/summary-card.ts
@@ -21,22 +21,29 @@ export class SummaryCard {
     percentageDisplay.innerHTML = "0%"; 
 
     let scoreDisplay = document.getElementsByClassName("score")[0];
-    scoreDisplay.innerHTML = "";
+    scoreDisplay.innerHTML = this.score > 0 ? "" : `${this.score}/${this.questionCount}`;
 
     let count = 0; 
 
     const interval = setInterval(() => {
-      if (count > percentage){
-        clearInterval(interval);
-        scoreDisplay.innerHTML = `${this.score}/${this.questionCount}`;
-      } else {
+      if (count < percentage){
         percentageDisplay.innerHTML = `${count.toFixed(0)}%`;
         document.documentElement.style.setProperty("--quiz-percentage", `${count}%`);
         document.documentElement.style.setProperty("--quiz-percentage-point", `${count + 0.1}%`);
         count += 0.2; 
+      } else {
+        clearInterval(interval);
+
+        if (percentage === 0){
+          document.documentElement.style.setProperty("--quiz-percentage", "0%");
+          document.documentElement.style.setProperty("--quiz-percentage-point", "0%");
+        } else if (percentage === 100){
+          document.documentElement.style.setProperty("--quiz-percentage", "100%");
+          document.documentElement.style.setProperty("--quiz-percentage-point", "100.1%");
+        }
+        
+        scoreDisplay.innerHTML = `${this.score}/${this.questionCount}`;
       }
     }, 0.4);
   };
-
-  
-}
+};
