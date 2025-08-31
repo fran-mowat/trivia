2df18947c5dae558621132a2d5c93e6be00177a4
fran0770 <franmowat33@gmail.com>
2025-08-31 16:19:06 +0100

    created unit tests for question service

--- src/app/services/question.service.spec.ts
+++ src/app/services/question.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuestionService } from './question.service';
+import { provideHttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('QuestionService', () => {
-  let service: QuestionService;
+  let questionService: QuestionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(QuestionService);
+    TestBed.configureTestingModule({ providers: [QuestionService, provideHttpClient()]});
+    questionService = TestBed.inject(QuestionService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(questionService).toBeTruthy();
+  });
+
+  it('should retrieve a token', (done) => {
+    questionService.getToken().subscribe(response => {
+      expect(response.response_code).toBe(0);
+      expect(response.response_message).toBe('Token Generated Successfully!');
+      expect(response.token).toBeTruthy();
+      done(); 
+    });
+  }); 
+
+  it('should retrieve questions', (done) => {
+    questionService.getQuestions('https://opentdb.com/api.php?type=multiple&amount=3', '').subscribe(response => {
+      expect(response.response_code).toBe(0);
+      expect(response.results.length).toBe(3);
+      done();
+    });
   });
 });
