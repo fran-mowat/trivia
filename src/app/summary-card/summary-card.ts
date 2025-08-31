2a2d7e037a217e227ed78486ca530628cc3f8ed6
fran0770 <franmowat33@gmail.com>
2025-08-31 19:44:52 +0100

    fixed aria-valuenow attribute error

--- src/app/summary-card/summary-card.ts
+++ src/app/summary-card/summary-card.ts
@@ -11,12 +11,15 @@ export class SummaryCard {
   @Input() questionCount!: number; 
   @Input() category!: string; 
   @Input() difficulty!: string;
+  percentage: number; 
 
   @Output() playAgain = new EventEmitter();
 
-  ngOnInit() {
-    let percentage = (this.score / this.questionCount) * 100;
+  constructor(){
+    this.percentage = (this.score / this.questionCount) * 100;
+  }
 
+  ngOnInit() {
     let percentageDisplay = document.getElementsByClassName("percentage")[0];
     percentageDisplay.innerHTML = "0%"; 
 
@@ -26,7 +29,7 @@ export class SummaryCard {
     let count = 0; 
 
     const interval = setInterval(() => {
-      if (count < percentage){
+      if (count < this.percentage){
         percentageDisplay.innerHTML = `${count.toFixed(0)}%`;
         document.documentElement.style.setProperty("--quiz-percentage", `${count}%`);
         document.documentElement.style.setProperty("--quiz-percentage-point", `${count + 0.1}%`);
@@ -34,10 +37,10 @@ export class SummaryCard {
       } else {
         clearInterval(interval);
 
-        if (percentage === 0){
+        if (this.percentage === 0){
           document.documentElement.style.setProperty("--quiz-percentage", "0%");
           document.documentElement.style.setProperty("--quiz-percentage-point", "0%");
-        } else if (percentage === 100){
+        } else if (this.percentage === 100){
           document.documentElement.style.setProperty("--quiz-percentage", "100%");
           document.documentElement.style.setProperty("--quiz-percentage-point", "100.1%");
         }
